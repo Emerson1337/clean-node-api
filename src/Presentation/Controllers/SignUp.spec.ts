397ebf125ab175c3bbe04d6d7f5acfba126a1c50
Emerson Lucena <emersonlucenaoficial@gmail.com>
2022-09-16 16:51:09 -0300

    test: ensure SignUpController returns 400 if no password confirmation is provided

--- src/Presentation/Controllers/SignUp.spec.ts
+++ src/Presentation/Controllers/SignUp.spec.ts
@@ -49,4 +49,22 @@ describe('SignUp controller', () => {
 		expect(httpResponse.statusCode).toBe(400);
 		expect(httpResponse.body).toEqual(new MissingParamError('password'));
 	});
+
+	test('Should return 400 if no password confirmation is provided', () => {
+		const sut = new SignUpController();
+		const httpRequest = {
+			body: {
+				email: 'any_email@mail.com',
+				name: 'any_name',
+				password: 'any_password',
+			},
+		};
+
+		const httpResponse = sut.handle(httpRequest);
+
+		expect(httpResponse.statusCode).toBe(400);
+		expect(httpResponse.body).toEqual(
+			new MissingParamError('passwordConfirmation')
+		);
+	});
 });
