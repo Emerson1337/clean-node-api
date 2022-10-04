3873a96bfaab9f690c7027ae9c95e5ea8b1afbb4
Emerson Lucena <emersonlucenaoficial@gmail.com>
2022-10-04 10:02:31 -0300

    test: ensure SignUpController returns 200 if valid data is provided

--- src/Presentation/Controllers/SignUp/SignUp.spec.ts
+++ src/Presentation/Controllers/SignUp/SignUp.spec.ts
@@ -240,4 +240,27 @@ describe('SignUp controller', () => {
 			password: 'any_password',
 		});
 	});
+
+	test('Should return 200 if valid data is provided', () => {
+		const { sut } = makeSut();
+
+		const httpRequest = {
+			body: {
+				email: 'valid_email@mail.com',
+				name: 'valid_name',
+				password: 'valid_password',
+				passwordConfirmation: 'valid_password',
+			},
+		};
+
+		const httpResponse = sut.handle(httpRequest);
+
+		expect(httpResponse.statusCode).toBe(200);
+		expect(httpResponse.body).toEqual({
+			id: 'valid_id',
+			email: 'valid_email@mail.com',
+			name: 'valid_name',
+			password: 'valid_password',
+		});
+	});
 });
