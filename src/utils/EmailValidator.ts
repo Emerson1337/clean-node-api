6cca7be9cb6e3a75303369f3e90cd114b718910a
Emerson Lucena <emersonlucenaoficial@gmail.com>
2022-10-17 09:45:44 -0300

    refactor: change EmailValidatorAdapater to EmailValidatorAdapter

--- src/utils/EmailValidator.ts
+++ src/utils/EmailValidator.ts
@@ -1,7 +1,7 @@
 import { EmailValidator } from '../Presentation/Protocols/EmailValidator';
 import validator from 'validator';
 
-export class EmailValidatorAdapater implements EmailValidator {
+export class EmailValidatorAdapter implements EmailValidator {
 	isValid(email: string): boolean {
 		return validator.isEmail(email);
 	}
