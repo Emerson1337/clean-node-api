e029bed026e9ea179c72fcf6c62ea59e22f1bc81
Emerson Lucena <emersonlucenaoficial@gmail.com>
2022-10-24 09:07:11 -0300

    feat: ensure DbAddAccount returns an account on success

--- src/Data/UseCases/AddAccount/DbAddAccount.ts
+++ src/Data/UseCases/AddAccount/DbAddAccount.ts
@@ -21,19 +21,12 @@ export class DbAddAccount implements AddAccount {
 	async add(accountData: AddAccountModel): Promise<AccountModel> {
 		const hashedPassword = await this.encrypter.encrypt(accountData.password);
 
-		await this.addAccountRepository.add(
+		const account = await this.addAccountRepository.add(
 			Object.assign({}, accountData, {
 				password: hashedPassword,
 			})
 		);
 
-		return new Promise((resolve) =>
-			resolve({
-				id: 'id',
-				name: 'test',
-				email: 'test@mail.com',
-				password: 'password',
-			})
-		);
+		return account;
 	}
 }
