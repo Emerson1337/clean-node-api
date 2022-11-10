eaad25f395522ee125972158627f183f7305dff7
Emerson Lucena <emersonlucenaoficial@gmail.com>
2022-11-10 10:24:34 -0300

    chore: exclude protocols and domain to be coverage by tests

--- jest.config.ts
+++ jest.config.ts
@@ -1,6 +1,11 @@
 module.exports = {
 	roots: ['<rootDir>/src'],
-	collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+	collectCoverageFrom: [
+		'<rootDir>/src/**/*.ts', 
+		'!**/Protocols/**.ts',
+		'!**/Domain/**/*.ts',
+		'!**/Protocols/**.ts',
+	],
 	coverageDirectory: 'coverage',
 	coverageProvider: 'v8',
 	testEnvironment: 'node',
