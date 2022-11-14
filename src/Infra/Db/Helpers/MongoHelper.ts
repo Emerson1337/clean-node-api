54c7b3f52046dbbe076da804f1ce157eb315722c
Emerson Lucena <emersonlucenaoficial@gmail.com>
2022-11-14 10:32:17 -0300

    refactor: change folder name and put first letter capitalized

---
+++ src/Infra/Db/Helpers/MongoHelper.ts
@@ -0,0 +1,16 @@
+import { MongoClient } from "mongodb";
+
+
+export const MongoHelper = {
+	client: null as unknown as MongoClient,
+
+	async connect(uri: string): Promise<void> {
+		if(process.env.MONGO_URL){
+			this.client = await MongoClient.connect(process.env.MONGO_URL);
+		}
+	},
+
+	async disconnect(): Promise<void> {
+		await this.client.close();
+	}
+};
