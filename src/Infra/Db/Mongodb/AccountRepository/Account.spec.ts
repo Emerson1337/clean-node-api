09c9a2a230433259715838c67cb4becfb19a7460
Emerson Lucena <emersonlucenaoficial@gmail.com>
2022-11-22 22:47:22 -0300

    test: ensure AccountMongoRepository returns an account on success

---
+++ src/Infra/Db/Mongodb/AccountRepository/Account.spec.ts
@@ -0,0 +1,27 @@
+import { MongoHelper } from '../../Helpers/MongoHelper';
+import { AccountMongoRepository } from './Account';
+
+describe('Account Mongo Repository', () => {
+	beforeAll(async () => {
+		process.env.MONGO_URL && (await MongoHelper.connect(process.env.MONGO_URL));
+	});
+
+	afterAll(async () => {
+		await MongoHelper.disconnect();
+	});
+
+	test('Should return an account on success', async () => {
+		const sut = new AccountMongoRepository();
+		const account = await sut.add({
+			name: 'any_name',
+			email: 'any_email@mail.com',
+			password: 'any_password',
+		});
+
+		expect(account).toBeTruthy();
+		expect(account.id).toBeTruthy();
+		expect(account.name).toBe('any_name');
+		expect(account.email).toBe('any_email@mail.com');
+		expect(account.password).toBe('any_password');
+	});
+});
