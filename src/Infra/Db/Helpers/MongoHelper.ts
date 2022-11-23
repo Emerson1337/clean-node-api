50ecb83d2632e9d351a2c412c1d3b012cd7f214e
Emerson Lucena <emersonlucenaoficial@gmail.com>
2022-11-22 23:08:27 -0300

    refactor: remove unused import and param

--- src/Infra/Db/Helpers/MongoHelper.ts
+++ src/Infra/Db/Helpers/MongoHelper.ts
@@ -1,12 +1,11 @@
 import { Collection, MongoClient } from 'mongodb';
-import { AccountModel } from '../../../Domain/Models';
 
 export const MongoHelper = {
 	client: null as unknown as MongoClient,
 
 	async connect(uri: string): Promise<void> {
 		if (process.env.MONGO_URL) {
-			this.client = await MongoClient.connect(process.env.MONGO_URL);
+			this.client = await MongoClient.connect(uri);
 		}
 	},
 
