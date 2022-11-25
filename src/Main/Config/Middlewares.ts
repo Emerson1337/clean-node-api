0e29cfc7045b7e6eb76f43b582c57ff354f1475d
Emerson Lucena <emersonlucenaoficial@gmail.com>
2022-11-25 15:06:03 -0300

    feat: ensure content type is json

--- src/Main/Config/Middlewares.ts
+++ src/Main/Config/Middlewares.ts
@@ -1,8 +1,10 @@
 import { bodyParser } from './../Middlewares/BodyParser';
 import { Express } from 'express';
 import { cors } from '../Middlewares/Cors';
+import { contentType } from '../Middlewares/ContentType';
 
 export default (app: Express): void => {
 	app.use(bodyParser);
 	app.use(cors);
+	app.use(contentType);
 };
