cfcb97e8af236380ed291467c8f9cdf613b50ff7
Emerson Lucena <emersonlucenaoficial@gmail.com>
2022-11-25 14:41:58 -0300

    feat: ensure cors is enabled

--- src/Main/Config/Middlewares.ts
+++ src/Main/Config/Middlewares.ts
@@ -1,6 +1,8 @@
 import { bodyParser } from './../Middlewares/BodyParser';
 import { Express } from 'express';
+import { cors } from '../Middlewares/Cors';
 
 export default (app: Express): void => {
 	app.use(bodyParser);
+	app.use(cors);
 };
