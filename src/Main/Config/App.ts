aace837b25b42377b8a9dfa89b61e5663c92359b
Emerson Lucena <emersonlucenaoficial@gmail.com>
2022-11-28 20:49:54 -0300

    feat: ensure SingUp route returns an accout on success

--- src/Main/Config/App.ts
+++ src/Main/Config/App.ts
@@ -1,8 +1,10 @@
 import express from 'express';
 import setupMiddlewares from './Middlewares';
+import setupRoutes from './Routes';
 
 const app = express();
 
 setupMiddlewares(app);
+setupRoutes(app);
 
 export default app;
